032fcfe5788e55f3c918c6d3f314beb3f0d74b5a
PATRICIAGANG <85884951+PATRICIAGANG@users.noreply.github.com>
2021-09-02 08:19:00 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+
+
+class Config(object):
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+
+    APP_ID = int(os.environ.get("APP_ID", ""))
+
+    API_HASH = os.environ.get("API_HASH", "")
+
+    AUDIO_THUMBNAIL = os.environ.get("AUDIO_THUMBNAIL", "https://telegra.ph/file/d2372fe42ccf8c9273088.jpg")
+
+    VIDEO_THUMBNAIL = os.environ.get("VIDEO_THUMBNAIL", "https://telegra.ph/file/d2372fe42ccf8c9273088.jpg")
